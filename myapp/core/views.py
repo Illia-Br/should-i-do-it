cf1bd30d7365a4aba4fe503f0c8cf48ba46d924f
Illia Brytun <elbrytun@gmail.com>
2022-04-11 16:03:36 -0400

    finish add post functionality

--- myapp/core/views.py
+++ myapp/core/views.py
@@ -1,13 +1,13 @@
-# core/views.py 
-
 from flask import render_template, request, Blueprint
+from myapp.models import Posts
 
 core = Blueprint('core', __name__)
 
 @core.route('/')
 def index():
-    
-    return render_template('index.html')
+    page = request.args.get('page', 1, type=int)
+    posts = Posts.query.order_by(Posts.date.desc()).paginate(page=page, per_page=5)
+    return render_template('index.html', posts=posts)
 
 @core.route('/info')
 def info():
