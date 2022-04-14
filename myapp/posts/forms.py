2206b28a35874afbc76478d0844c123936cebf21
Illia Brytun <elbrytun@gmail.com>
2022-04-14 10:23:06 -0400

    style create post page

--- myapp/posts/forms.py
+++ myapp/posts/forms.py
@@ -3,6 +3,6 @@ from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
 
 class PostForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
+    title = StringField('Your question starts with "Should I...', validators=[DataRequired()])
     description = TextAreaField('Description', validators=[DataRequired()])
-    submit = SubmitField('Post')
+    submit = SubmitField('Add Question')
