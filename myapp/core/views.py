940ce08472d517b7772c0230e9d8129b6faabf4b
Illia Brytun <elbrytun@gmail.com>
2022-04-13 23:11:42 -0400

    add styling for the home page

--- myapp/core/views.py
+++ myapp/core/views.py
@@ -6,7 +6,7 @@ core = Blueprint('core', __name__)
 @core.route('/')
 def index():
     page = request.args.get('page', 1, type=int)
-    posts = Post.query.order_by(Post.date.desc()).paginate(page=page, per_page=7)
+    posts = Post.query.order_by(Post.date.desc()).paginate(page=page, per_page=5)
     return render_template('index.html', posts=posts)
 
 @core.route('/info')
