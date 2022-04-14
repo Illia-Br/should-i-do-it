7be1dcf25533755cd89d32e5fd7c8da5e2411253
Illia Brytun <elbrytun@gmail.com>
2022-04-14 17:41:13 -0400

    add minor bug fixes

--- myapp/core/views.py
+++ myapp/core/views.py
@@ -4,11 +4,11 @@ from myapp.models import Post
 core = Blueprint('core', __name__)
 
 @core.route('/')
+def info():
+    return render_template('info.html')
+
+@core.route('/allPosts')
 def index():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.date.desc()).paginate(page=page, per_page=5)
     return render_template('index.html', posts=posts)
-
-@core.route('/info')
-def info():
-    return render_template('info.html')
